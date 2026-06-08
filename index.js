3348bc85960e7e6b6c9ec3b6d1dcb1cd85cd67c4
Quincy-Bee <quincybee@icloud.com>
2026-06-08 01:29:08 -0400

    get user data

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-function getUserData(id) {
-  const dbs = {
-    db1: db1,
-    db2: db2,
-    db3: db3
-  };
+async function getUserData(id) {
+    const dbs = {
+        db1: db1,
+        db2: db2,
+        db3: db3
+    };
+
+    try {
+        // Find which database contains the user
+        const dbName = await central(id);
+
+        // Fetch user data
+        const [dbData, vaultData] = await Promise.all([
+            dbs[dbName](id),
+            vault(id)
+        ]);
+
+        // Merge the data into the object
+        return {
+            id,
+            ...vaultData,
+            ...dbData
+        };
+
+    } catch (error) {
+        throw error;
+    }
 }
 
-try {
-    const dbName = await central(id):
+getUserData(1)
+    .then((user) => console.log(user))
+    .catch((err) => console.error(err));
 
-    const [dbData, valutData] = await Promise.all([
-        dbs][dbName](id),
-        vault(id)
-    ]);
-
-    
-
-}
+getUserData(11)
+    .then((user) => console.log(user))
+    .catch((err) => console.error(err));
